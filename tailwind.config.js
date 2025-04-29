050bac4c2590cd8b7d776451ce81b224a7f953ea
AdrianLona <adrian.lona@mscsdirect.com>
2025-04-29 08:05:35 -0600

    modals name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html", "./modas-update.html"],
+  content: ["./src/**/*.{html,js}", "./index.html", "./modals-update.html"],
   theme: {
     extend: {},
   },
