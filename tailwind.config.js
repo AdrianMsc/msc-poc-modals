4e49ad15a66f6c8eb122eda25ede58f18b2dc98f
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-05-05 10:32:29 -0600

    new pdp envirnoment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html", "./modals-update.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    // "./index.html",
+    "./modals-update.html",
+  ],
   theme: {
     extend: {},
   },
